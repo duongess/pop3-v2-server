fa5b3723e71474ceb308cfc2d7a2375d897daf77
duongess <duongcantat@gmail.com>
2025-10-15 00:16:41 +0700

    fix server

--- server/utils.cpp
+++ server/utils.cpp
@@ -4,34 +4,34 @@
 void menuServer() {
     std::cout << "\n=== Network System Menu ===\n";
     std::cout << "1) Start TCP service\n";
+    std::cout << "c) Create account\n";
+    std::cout << "j) Join account\n";
     std::cout << "q) Quit\n> ";
     return;
 }
 
 void registerServer(Server &server) {
     std::string username, password;
-    while (true) {
-        if (!server.hasAnyUser()) {
-        std::cout << "Register a server account" << std::endl;
-        std::cout << "Server Name: "; std::getline(std::cin, username);
-        std::cout << "Server Password: "; std::getline(std::cin, password);
-        server.signUp(username, password);
-        std::cout << "___________________" << std::endl;
-        } else {
-        std::cout << "Log in: " << std::endl;
-        std::cout << "Server Name: "; std::getline(std::cin, username);
-        std::cout << "Server Password: "; std::getline(std::cin, password);
-        AuthResult status = server.signIn(username, password);
-        switch (status) {
-            case AuthResult::Ok :
-                std::cout << "Log in OK " << std::endl;
-                return;
-            case AuthResult::UserNotFound:
-            case AuthResult::WrongPassword:
-                std::cout << "Log in failure " << std::endl;
-                break;
-            }
-        }
+    std::cout << "Register a server account" << std::endl;
+    std::cout << "Server Name: "; std::getline(std::cin, username);
+    std::cout << "Server Password: "; std::getline(std::cin, password);
+    server.signUp(username, password);
+}
+
+void joinServer(Server &server) {
+    std::string username, password;
+    std::cout << "Log in: " << std::endl;
+    std::cout << "Server Name: "; std::getline(std::cin, username);
+    std::cout << "Server Password: "; std::getline(std::cin, password);
+    AuthResult status = server.signIn(username, password);
+    switch (status) {
+        case AuthResult::Ok :
+            std::cout << "Log in OK " << std::endl;
+            return;
+        case AuthResult::UserNotFound:
+        case AuthResult::WrongPassword:
+            std::cout << "Log in failure " << std::endl;
+            break;
     }
-    return;
+    server.checkAccout();
 }
