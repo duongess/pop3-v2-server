ba39fff216b2d860e756936fcebb74c18727a2f5
donaldsam001 <nongvansam07072005@gmail.com>
2025-10-19 22:07:40 +0700

    message

--- server/storage/mail.cpp
+++ server/storage/mail.cpp
@@ -1,7 +1,8 @@
 #include "mail.h"
 
-bool MailTable::createTableIfNeeded() {
-    static const char* sql =
+bool MailTable::createTableIfNeeded()
+{
+    static const char *sql =
         "CREATE TABLE IF NOT EXISTS emails ("
         "  mailId INTEGER PRIMARY KEY AUTOINCREMENT,"
         "  userId INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,"
@@ -12,4 +13,19 @@ bool MailTable::createTableIfNeeded() {
         "  receivedAt INTEGER NOT NULL"
         ");";
     return exec_sql(conn_.get(), sql, "Emails");
+}
+
+bool MailTable::setFlag(int mailId, const std::string &flag)
+{
+    const char *sql = "UPDATE emails SET flags = ? WHERE mailId = ?";
+    sqlite3_stmt *stmt;
+    if (sqlite3_prepare_v2(conn_.get(), sql, -1, &stmt, nullptr) != SQLITE_OK)
+        return false;
+
+    sqlite3_bind_text(stmt, 1, flag.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_int(stmt, 2, mailId);
+
+    bool ok = (sqlite3_step(stmt) == SQLITE_DONE);
+    sqlite3_finalize(stmt);
+    return ok;
 }
