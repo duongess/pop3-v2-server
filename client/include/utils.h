e5312d2abb693fa4ace9061234e8ece1b19cbcef
duongess <duongcantat@gmail.com>
2025-10-26 00:02:20 +0700

    update only server

--- client/include/utils.h
+++
@@ -1,7 +0,0 @@
-#include "common/utils.h"
-#include "types/clientInput.h"
-
-void menuClient();
-void menuPop3v2();
-void accountInformation(const std::string& username, const std::string& host, const std::string& port);
-ParsedCommand parseCliLine(std::string_view line);
