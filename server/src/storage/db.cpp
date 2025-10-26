ed1ac23172bfe0bc092bf58260df2b9c3a01cfa2
duongess <duongcantat@gmail.com>
2025-10-26 23:06:33 +0700

    test /usr/app/database/POP3V2.db

--- server/src/storage/db.cpp
+++ server/src/storage/db.cpp
@@ -1,11 +1,11 @@
 #include "db.h"
-const std::string DB_DIR = (env == "DEVELOPMENT") ? "database/" : "/app/database/";
+const std::string DB_DIR = (env == "DEVELOPMENT") ? "database/" : "/usr/app/database/";
 DB::DB() 
     : conn(
         // Logic chọn path file DB
         (env == "DEVELOPMENT") 
             ? "database/POP3V2.dev.db" 
-            : "/app/database/POP3V2.db"
+            : "/usr/app/database/POP3V2.db"
       ),
       // KHỞI TẠO USER VÀ MAIL BẰNG OBJECT 'conn' đã được tạo
       user(conn), 
